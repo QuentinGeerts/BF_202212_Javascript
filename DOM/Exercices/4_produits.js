36ff8446fe4d81bc6b29a5ad7115c0c8a2ac1f7f
Quentin <quentin.geerts@bstorm.be>
2022-12-06 14:58:21 +0100

    Fin exercice shopping list amélioré

--- DOM/Exercices/4_produits.js
+++ DOM/Exercices/4_produits.js
@@ -3,6 +3,8 @@
 window.onload = () => {
     const buttons = document.querySelectorAll('.add-to-cart');
 
+    document.getElementById('total').innerText = 0;
+
     for (const button of buttons) {
         // e : événement déclenché
         button.onclick = (monEvent) => {
@@ -50,7 +52,7 @@ function addToCart (product) {
         const productName = document.createElement('td');
         const productPrice = document.createElement('td');
         const productQuantity = document.createElement('td');
-        const productQuantityInput = document.createElement('input')
+        const productQuantityInput = document.createElement('input');
         const productRemove = document.createElement('td');
         const productRemoveBtn = document.createElement('button');
 
@@ -59,17 +61,61 @@ function addToCart (product) {
         productRow.appendChild(productName);
         productRow.appendChild(productPrice);
         productRow.appendChild(productQuantity);
-        productQuantity.appendChild(productQuantityInput)
+        productQuantity.appendChild(productQuantityInput);
         productRow.appendChild(productRemove);
         productRemove.appendChild(productRemoveBtn);
 
         // Paramétrer les éléments HTML
         productName.innerText = product.productName;
+
         productPrice.innerText = product.productPrice;
-        productQuantityInput.setAttribute("type", "number")
-        productQuantityInput.setAttribute("min", "0")
-        productQuantityInput.value = product.productQuantity
+
+        productQuantityInput.setAttribute("type", "number");
+        productQuantityInput.setAttribute("min", "1");
+        productQuantityInput.value = product.productQuantity;
+        productQuantityInput.addEventListener('input', (e) => {            
+            updateTotal()            
+            if (e.target.value == 0 && e.target.value != "") removeToCart(e)
+            
+        });
+
         productRemoveBtn.innerText = "❌";
+        productRemoveBtn.addEventListener('click', removeToCart);
+
     }
 
+    // Mise à jour du total dans le cas :
+    // • d'un ajout de produit qui n'est pas dans la liste
+    // • mise à jour d'un produit qui est dans la liste
+    const total = document.getElementById('total');
+    total.innerText = +total.innerText + (+product.productPrice * +product.productQuantity);
+}
+
+function removeToCart (event) {
+    const productRow = event.target.parentNode.parentNode;
+
+    let price = productRow.children[1].innerText;
+    let quantity = productRow.children[2].children[0].value;
+
+    const total = document.getElementById('total');
+    total.innerText = +total.innerText - (+price * +quantity);
+
+    productRow.remove();
+}
+
+function updateTotal () {
+
+    const cart = document.getElementById('panier');
+    const totalCart = document.getElementById('total');
+
+    let total = 0;
+
+    for (const productRow of cart.children) {
+        let price = productRow.children[1].innerText;
+        let quantity = productRow.children[2].children[0].value;
+
+        total += +price * +quantity;
+    }
+
+    totalCart.innerText = total;
 }
